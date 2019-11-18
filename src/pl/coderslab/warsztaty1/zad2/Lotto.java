fa9aec0de0f29a5019851feefc4e382ec73836f5
Gottskalksson <adam.rolirad@gmail.com>
2019-11-18 20:06:31 +0100

    Lotto - task finished

--- src/pl/coderslab/warsztaty1/zad2/Lotto.java
+++ src/pl/coderslab/warsztaty1/zad2/Lotto.java
@@ -9,32 +9,17 @@ public class Lotto {
         System.out.println(drawingResult());
     }
 
-    public static int[] chosenNumber() {
-        Scanner scan = new Scanner(System.in);
+    private static int[] userDraw() {
         int[] tab = new int[6];
         System.out.println("Write down numbers between 1-49. Press Enter after write every number. " +
                 "Remember, every number must be only once.");
-
-//        int index = 0;
-//        while (index < 6) {
-//            tab [index] = -1;
-//            while (!(tab[index] > 0 && tab[index] < 50)) {
-//                try {
-//                    tab[index] = Integer.parseInt(scan.nextLine());
-//                    while (tab[index] > 49 || tab[index] < 1) {
-//                        System.out.println("You wrote incorrect number. Please write down new number between 1-49.");
-//                        tab[index] = Integer.parseInt(scan.nextLine());
-//                    }
-//                } catch (NumberFormatException e) {
-//                    System.out.println("You didn't write a number! Try again:");
-//                }
-//            }
-//            index++;
-//        }
-
-        tabWithChosenNumber(tab);
-
+        // user fills their coupon
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = userNumber();
+        }
         Arrays.sort(tab);
+
+        //checked any repeats in user's coupon
         int errors;
 
         do {
@@ -45,7 +30,7 @@ public class Lotto {
                         while (tab[i] == tab[j]) {
                             errors++;
                             System.out.println("Number " + tab[i] + " is more than once. Write another number.");
-                            tab[j] = scan.nextInt();
+                            tab[j] = userNumber();
                         }
                     }
                 }
@@ -54,11 +39,28 @@ public class Lotto {
         } while (errors != 0);
         System.out.println("Your numbers: ");
         System.out.println(Arrays.toString(tab));
-
         return tab;
     }
 
-    public static Integer[] drewNumber() {
+    private static int userNumber() {
+        Scanner scan = new Scanner(System.in);
+        int num = 0;
+        while (num < 1 || num > 49) {
+            try {
+                num = Integer.parseInt(scan.nextLine());
+                while (num < 1 || num > 49) {
+                    System.out.println("You wrote incorrect number. Please write down new number between 1-49.");
+                    num = Integer.parseInt(scan.nextLine());
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("You didn't write a number! Try again:");
+            }
+        }
+        return num;
+    }
+
+    //computer draws lucky numbers
+    private static Integer[] drewNumber() {
         Integer[] arr = new Integer[49];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
@@ -72,8 +74,9 @@ public class Lotto {
         return result;
     }
 
-    public static String drawingResult() {
-        int[] tab = chosenNumber();
+    //here computer checked user coupon with lucky numbers and return result of drawing
+    private static String drawingResult() {
+        int[] tab = userDraw();
         Integer[] drew = drewNumber();
 
         int result = 0;
@@ -88,24 +91,8 @@ public class Lotto {
         if (result > 2) {
             return "Congratulations! You have today " + result + " numbers of 6!";
         } else {
-            return "Unfortunately you have today " + result + " numbers of 6. You won nothing.";
+            return "Unfortunately you have today only " + result + " number(s) of 6. You won nothing.";
         }
-    }
-
-    public static int [] tabWithChosenNumber (int [] tab) {
-        int index = 0;
-        while (!(tab[index] > 0 && tab[index] < 50)) {
-            try {
-                tab[index] = Integer.parseInt(scan.nextLine());
-                while (tab[index] > 49 || tab[index] < 1) {
-                    System.out.println("You wrote incorrect number. Please write down new number between 1-49.");
-                    tab[index] = Integer.parseInt(scan.nextLine());
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("You didn't write a number! Try again:");
-            }
-            index++;
-        }
-        return tab;
     }
 }
+
